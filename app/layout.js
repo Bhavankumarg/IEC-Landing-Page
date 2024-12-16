8a2dffcf694ee644002f2eff2253e9de23bcbf3c
Bhavankumarg <gbhavanbkr@gmail.com>
2024-12-16 14:21:19 +0530

    robots and sitemap.xml updation

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import Footer from "@/components/Footer/Footer";
 import "@/styles/globals.css";
-import AOS from 'aos';
-import 'aos/dist/aos.css';  // Importing the CSS file
+import AOS from "aos";
+import "aos/dist/aos.css"; // Importing the CSS file
+import Script from "next/script";
 
 // const seoField = {
 //     title: "Integrated solutions to shape social equity | CMS",
@@ -11,12 +12,21 @@ import 'aos/dist/aos.css';  // Importing the CSS file
 //     pageType: "WebSite"
 //   };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-     
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DJZ93DKLRP"
+        ></Script>
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-DJZ93DKLRP');`}
+        </Script>
+      </head>
       <body>
         {/* <Header/> */}
         {children}
